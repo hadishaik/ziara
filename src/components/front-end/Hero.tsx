70011a805104dc5092f341e85283a1d5d229ed8a
shaikhaji123 <haji.shaik@troinfotech.in>
2024-07-29 11:24:20 +0530

    videoMuted

--- src/components/front-end/Hero.tsx
+++ src/components/front-end/Hero.tsx
@@ -5,7 +5,12 @@ const Hero = () => {
   return (
     <div className="h-auto ">
       <div className="">
-        <video autoPlay loop className="-z-10 object-cover aspect-video w-full">
+        <video
+          autoPlay
+          loop
+          muted
+          className="-z-10 object-cover aspect-video w-full"
+        >
           <source
             type="video/mp4"
             src="https://res.cloudinary.com/de4nrkdsz/video/upload/v1721739929/Untitled_design_r7ynpy.mp4"
